70ab7e15c26acdce247b440fa4ff6eddb4207955
brucean52 <bruce.an52@gmail.com>
2018-01-09 00:38:23 -0800

    updated the skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -4,11 +4,19 @@ import skillsData from '../assets/data/skills_data';
 
 import htmlLogo from '../assets/images/skills/html_logo.png';
 import cssLogo from '../assets/images/skills/css_logo.png';
-import jsLogo from '../assets/images/skills/javascript_logo.png';
-import reactLogo from '../assets/images/skills/react-logo.png';
 
-import nodephpLogo from '../assets/images/skills/node_php.png';
-import databaseLogo from '../assets/images/skills/database.png';
+import jsLogo from '../assets/images/skills/javascript_logo.png';
+import oopLogo from '../assets/images/skills/oop.png';
+
+import reactLogo from '../assets/images/skills/react-logo.png';
+import reduxLogo from '../assets/images/skills/redux.png';
+
+//import nodephpLogo from '../assets/images/skills/node_php.png';
+//import databaseLogo from '../assets/images/skills/database.png';
+
+import agileLogo from '../assets/images/skills/agile.png';
+import vscodeLogo from '../assets/images/skills/vs_code.jpg';
+
 import jqueryajaxLogo from '../assets/images/skills/jquery_ajax2.png';
 import apiLogo from '../assets/images/skills/api.png';
 
@@ -34,7 +42,7 @@ export default props =>{
                     <div className="col s12 l6 skills">
                         <img className="col s3" src={jsLogo} alt=""/>
 
-                        <img id="reactLogo" className="col s3 l3" src={reactLogo} alt=""/>
+                        <img id="reactLogo" className="col s3 l3" src={oopLogo} alt=""/>
                         <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius!
                         </div>  
                     </div>
@@ -43,16 +51,16 @@ export default props =>{
 
                 <div className="row">
                     <div className="col s12 l6 skills">
-                        <img className="col s3" src={nodephpLogo} alt=""/>
+                        <img className="col s3" src={jqueryajaxLogo} alt=""/>
 
-                        <img className="col s3" src={databaseLogo} alt=""/>
+                        <img className="col s3" src={apiLogo} alt=""/>
                         <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt.
                         </div>
                     </div>
                     
                     <div className="col s12 l6 skills">
-                        <img id="jqueryLogo" className="col s6 l3" src={jqueryajaxLogo} alt=""/>
-                        <img id="apiLogo" className="col s6 l3" src={apiLogo} alt=""/>
+                        <img id="jqueryLogo" className="col s6 l3" src={reactLogo} alt=""/>
+                        <img id="apiLogo" className="col s6 l3" src={reduxLogo} alt=""/>
                         <div className="col s12 l6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, explicabo!
                         </div>
                     </div>
@@ -61,9 +69,9 @@ export default props =>{
                                 
                 <div className="row">
                     <div className="col s12 l6 skills">
-                        <img className="col s3" src={nodephpLogo} alt=""/>
+                        <img className="col s3" src={agileLogo} alt=""/>
 
-                        <img className="col s3" src={databaseLogo} alt=""/>
+                        <img className="col s3" src={vscodeLogo} alt=""/>
                         <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt.
                         </div>
                     </div>
