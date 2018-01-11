8cd4d629234d09696d49c8e6afeaec038f256550
brucean52 <bruce.an52@gmail.com>
2018-01-11 15:43:45 -0800

    skills section is dynamic

---
+++ src/assets/data/skills_data.js
@@ -0,0 +1,59 @@
+import htmlLogo from '../images/skills/html_logo.png';
+import cssLogo from '../images/skills/css_logo.png';
+
+import jsLogo from '../images/skills/javascript_logo.png';
+import oopLogo from '../images/skills/oop.png';
+
+import jqueryajaxLogo from '../images/skills/jquery_ajax2.png';
+import apiLogo from '../images/skills/api.png';
+
+import reactLogo from '../images/skills/react-logo.png';
+import reduxLogo from '../images/skills/redux.png';
+
+//import nodephpLogo from '../images/skills/node_php.png';
+//import databaseLogo from '../images/skills/database.png';
+
+import agileLogo from '../images/skills/agile.png';
+import vscodeLogo from '../images/skills/vs_code.jpg';
+
+import responsiveDesign from '../images/skills/responsive-design.png';
+import bootstrapPs from '../images/skills/bootstrap_ps.png';
+
+export default[
+    {
+        img_11: htmlLogo,
+        img_11_alt: 'HTML',
+        img_12: cssLogo,
+        img_12_alt: 'CSS',
+        text_1: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!',
+        img_21: jsLogo,
+        img_21_alt: 'Javascript',
+        img_22: oopLogo,
+        img_22_alt: 'OOP',
+        text_2: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!'
+    },
+    {
+        img_11: jqueryajaxLogo,
+        img_11_alt: 'JQuery Ajax',
+        img_12: apiLogo,
+        img_12_alt: 'API',
+        text_1: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!',
+        img_21: reactLogo,
+        img_21_alt: 'React',
+        img_22: reduxLogo,
+        img_22_alt: 'Redux',
+        text_2: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!'
+    },
+    {
+        img_11: agileLogo,
+        img_11_alt: 'Agile',
+        img_12: vscodeLogo,
+        img_12_alt: 'VS Code',
+        text_1: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!',
+        img_21: responsiveDesign,
+        img_21_alt: 'Responsive Design',
+        img_22: bootstrapPs,
+        img_22_alt: 'BootStrap, PhotoShop',
+        text_2: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam, tenetur!'
+    },
+]
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,89 +1,22 @@
 import React from 'react';
+import SkillsRow from './skills_row';
 
 import skillsData from '../assets/data/skills_data';
 
-import htmlLogo from '../assets/images/skills/html_logo.png';
-import cssLogo from '../assets/images/skills/css_logo.png';
-
-import jsLogo from '../assets/images/skills/javascript_logo.png';
-import oopLogo from '../assets/images/skills/oop.png';
-
-import reactLogo from '../assets/images/skills/react-logo.png';
-import reduxLogo from '../assets/images/skills/redux.png';
-
-//import nodephpLogo from '../assets/images/skills/node_php.png';
-//import databaseLogo from '../assets/images/skills/database.png';
-
-import agileLogo from '../assets/images/skills/agile.png';
-import vscodeLogo from '../assets/images/skills/vs_code.jpg';
-
-import jqueryajaxLogo from '../assets/images/skills/jquery_ajax2.png';
-import apiLogo from '../assets/images/skills/api.png';
-
-import responsiveDesign from '../assets/images/skills/responsive-design.png';
-import bootstrap_ps from '../assets/images/skills/bootstrap_ps.png';
-
 export default props =>{
+    //map the skills section
+    const skillsList = skillsData.map((item, index)=>{
+        return(
+            <SkillsRow key={index} data={item}/>
+        );
+    });
+
     return(
         <section id='skills'>
             <h3>Technical Skills</h3>
             <hr/>
             <div className="container">
-
-                <div className="row">
-                    <div className="col s12 l6 skills">
-                        <img className="col s3" src={htmlLogo} alt=""/>
-
-                        <img id="cssLogo" className="col s3 l3" src={cssLogo} alt=""/>
-                        <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, deserunt!
-                        </div>
-                    </div>
-                    
-                    <div className="col s12 l6 skills">
-                        <img className="col s3" src={jsLogo} alt=""/>
-
-                        <img id="" className="col s3 l3" src={oopLogo} alt=""/>
-                        <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius!
-                        </div>  
-                    </div>
-
-                </div>
-
-                <div className="row">
-                    <div className="col s12 l6 skills">
-                        <img className="col s3" src={jqueryajaxLogo} alt=""/>
-
-                        <img className="col s3" src={apiLogo} alt=""/>
-                        <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt.
-                        </div>
-                    </div>
-                    
-                    <div className="col s12 l6 skills">
-                        <img id="" className="col s6 l3" src={reactLogo} alt=""/>
-                        <img id="" className="col s6 l3" src={reduxLogo} alt=""/>
-                        <div className="col s12 l6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, explicabo!
-                        </div>
-                    </div>
-
-                </div>  
-                                
-                <div className="row">
-                    <div className="col s12 l6 skills">
-                        <img className="col s3" src={agileLogo} alt=""/>
-
-                        <img className="col s3" src={vscodeLogo} alt=""/>
-                        <div className="col s6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nesciunt.
-                        </div>
-                    </div>
-                    
-                    <div className="col s12 l6 skills">
-                        <img className="col s6 l3" src={responsiveDesign} alt=""/>
-                        <img id="" className="col s6 l3" src={bootstrap_ps} alt=""/>
-                        <div className="col s12 l6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, explicabo!
-                        </div>
-                    </div>
-                    
-                </div>   
+                {skillsList}
             </div>
         </section>
     );
