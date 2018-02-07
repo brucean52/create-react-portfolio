94d3ade457409a66259b83b074df8d018bdd3284
brucean52 <bruce.an52@gmail.com>
2018-02-07 15:42:35 -0800

    included changes to the server for serving react files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
-//import 'babel-polyfill'
 const express = require('express');
-//import express from 'express'
 const bodyParser = require('body-parser');
-//import bodyParser from 'body-parser'
+const path = require('path');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const config = require("./config");
@@ -15,6 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
@@ -23,19 +23,15 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-app.get('*', (req, res) => {
-  res.send('Server is working. Please post at "/contact" to submit a message.')
-})
-
 app.post('/contact', (req, res) => {
   const { email, name, subject, message} = req.body
   console.log('req.body', req.body);
 
   const mailOptions = {
-    from: 'bruce.dev89@gmail.com',         // Sender of the email
+    from: email,         // Sender of the email
     to: 'bruce.an52@gmail.com',  // Recipient of the email
-    subject: 'Contact Form Response',              // Subject of the email
-    text: 'test',                // Message of the email
+    subject: `${subject} - ${name}`,              // Subject of the email
+    text: message,                // Message of the email
 
   };
   
@@ -57,7 +53,11 @@ app.post('/contact', (req, res) => {
   //   res.redirect('/#error');
   // })
 
-})
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 4000;
 
