ad265727e9e3fc11759c5a9b939a3d43dced1c68
brucean52 <bruce.an52@gmail.com>
2018-02-12 12:25:38 -0800

    skills tag update

--- client/src/components/skills.js
+++ client/src/components/skills.js
@@ -98,7 +98,7 @@ export default props =>{
                 
                 <div className="col s12 l6 skills">
 
-                <div className="col s6 push-l6">Mobile Responsive Design using libraries like <strong>Bootstrap</strong>, <strong>Material Design</strong>. Image editing experience using Adobe <strong>Photoshop</strong> and <strong>Illustrator</strong>.</div>  
+                <div className="col s6 push-l6"><strong>Mobile Responsive Design</strong> using libraries like <strong>Bootstrap</strong>, <strong>Material Design</strong>. Image editing experience using Adobe <strong>Photoshop</strong> and <strong>Illustrator</strong>.</div>  
                     <img className='col s3 pull-l6 responsiveLogo' src={responsiveDesign} alt='Responsive Design'/>
 
                     <img className='col s3 pull-l6 bootstrapLogo'  src={bootstrapPs} alt='BootStrap Material Design PhotoShop Illustrator'/>
