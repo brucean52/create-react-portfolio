077db0afc9450213d0525c18d1c77dccaeb56553
brucean52 <bruce.an52@gmail.com>
2018-03-06 17:20:28 -0800

    check for whitespace only contact and removed demo for portfolio

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default props =>{
+    let demoBtn = null;
+    if(props.demo !== ""){
+        demoBtn = (<a className="waves-effect waves-light btn" target="_blank" href={props.demo}><i className="material-icons right">present_to_all</i> demo</a>);
+    } 
     return(
     <div className="card col m6 s12 l4">
         <div className="card-image waves-effect waves-block waves-light">
@@ -9,14 +13,14 @@ export default props =>{
         <div className="card-content">
             <span className="card-title activator">{props.title}<i className="material-icons right">add</i></span>
             <a className="waves-effect waves-light btn" target="_blank" href={props.code}><i className="material-icons right">developer_mode</i> code</a>
-            <a className="waves-effect waves-light btn" target="_blank" href={props.demo}><i className="material-icons right">present_to_all</i> demo</a>        
+            {demoBtn}        
         </div>
 
         <div className="card-reveal">
             <span className="card-title card-open">{props.title}<i className="material-icons right">close</i></span>
             <p>{props.info}</p>
             <a className="waves-effect waves-light btn" target="_blank" href={props.code}><i className="material-icons right">developer_mode</i> code</a>
-            <a className="waves-effect waves-light btn" target="_blank" href={props.demo}><i className="material-icons right">present_to_all</i> demo</a>  
+            {demoBtn}
         </div>
     </div>
     );
